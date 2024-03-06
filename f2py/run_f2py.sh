8aa6047e6043e9909d2330c5a0173570d7baca70
Jiang Cao <jiang.cao89@gmail.com>
2024-03-06 10:53:18 +0100

    script

--- f2py/run_f2py.sh
+++ f2py/run_f2py.sh
@@ -1,6 +1,6 @@
 F90FLAGS="-ffree-line-length-none -fbounds-check -fbacktrace -fopenmp"
 
 gfortran -c mkl_dfti.f90
-f2py -c --fcompiler=gnu95 -m util src/util.f95 --f90flags="${F90FLAGS}"
+f2py -c --fcompiler=gnu95 -m util src/util.f95 --f90flags="${F90FLAGS}" 
 f2py -c --fcompiler=gnu95 -m wannier src/wannier.f95 --f90flags="${F90FLAGS}"
-f2py -c --fcompiler=gnu95 -m negf src/negf.f95 --f90flags="${F90FLAGS}"
+f2py -c --fcompiler=gnu95 -m negf src/negf.f95  skip: trimul_c mul_c : --f90flags="${F90FLAGS}"
