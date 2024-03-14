f5aee15f3cf06988b1264cc5c7976cb7b0f9cf56
Jiang Cao <jiang.cao89@gmail.com>
2024-03-14 18:26:59 +0100

    bse fullsolve and solve agree when ndiag=0

--- f2py/src/negf.f95
+++ f2py/src/negf.f95
@@ -1669,7 +1669,7 @@ module bse_dense
         real(dp) :: start, finish
         integer :: N,i,j,k,l,p,q,ie,row,col, table(2,nm_dev*nm_dev),it
         !          
-        N = nm_dev*nm_dev
+        ! N = nm_dev*nm_dev
         ! construct the table of reordered indices   
         it=0
         ! first put the i=j        
@@ -1689,20 +1689,21 @@ module bse_dense
             enddo
         enddo
         ! then put the others, but outside ndiag
-        do i=1,nm_dev
-            do j=1,nm_dev
-                if (i/=j) then 
-                    if (abs(i-j)>ndiag) then
-                        it=it+1
-                        table(:,it) = (/i,j/)
-                    endif
-                endif                    
-            enddo
-        enddo
-        if (it/=N) then 
-            print *, 'ERROR!'
-            call abort
-        endif
+        ! do i=1,nm_dev
+        !     do j=1,nm_dev
+        !         if (i/=j) then 
+        !             if (abs(i-j)>ndiag) then
+        !                 it=it+1
+        !                 table(:,it) = (/i,j/)
+        !             endif
+        !         endif                    
+        !     enddo
+        ! enddo
+        ! if (it/=N) then 
+        !     print *, 'ERROR!'
+        !     call abort
+        ! endif
+        N = it
         ! start computation
         allocate(Mmat(N,N), source=czero)        
         allocate(Lmat(N,N), source=czero)     
@@ -1918,14 +1919,15 @@ module bse_dense
             do j=1,nm_dev      
                 row= (i-1)*nm_dev + i                
                 col= (j-1)*nm_dev + j
-                P_retarded(i,j) = - c1i * Mmat(row,col)                
+                P_retarded(i,j) = - c1i * Mmat(row,col)            
+                Amat(i,j) = Lmat(row,col)   
             enddo
         enddo
         !$omp end do
         !$omp end parallel
         !
         ! ! calculate RPA epsilon and output to file
-        call zgemm('n','n',nm_dev,nm_dev,nm_dev,c1i,V,nm_dev,Lmat(1:nm_dev,1:nm_dev),nm_dev,czero,epsilon_M,nm_dev) 
+        call zgemm('n','n',nm_dev,nm_dev,nm_dev,c1i,V,nm_dev,Amat(1:nm_dev,1:nm_dev),nm_dev,czero,epsilon_M,nm_dev) 
         do j=1,nm_dev 
             epsilon_M(j,j) = epsilon_M(j,j) + cone
         enddo      
@@ -1962,7 +1964,7 @@ module bse_dense
         integer :: ie,i,j,k,l,m,n,p
         integer :: nn,nm,pp,pq
         complex(dp),allocatable :: L0xx(:,:),A(:,:),B(:,:),Kxx(:,:), Mxx(:,:), Sxx(:,:)
-        complex(dp) :: Qijkl, L0Kdd
+        complex(dp) :: Qijkl, L0Kdd, epsM
         real(dp) :: dE  
         real(dp) :: weights(nsub), xen(nsub)
         integer :: isub, ik, ikd
@@ -1974,12 +1976,11 @@ module bse_dense
         ik=1
         ikd=1
         !
-        allocate( L0xx(nm_dev,nm_dev) )
-        allocate( Mxx(nm_dev,nm_dev) )
-        allocate( Sxx(nm_dev,nm_dev) )
-        allocate( Kxx(nm_dev,nm_dev) )
-        allocate( A(nm_dev,nm_dev) )
-        L0xx = czero        
+        allocate( L0xx(nm_dev,nm_dev) , source=czero)
+        allocate( Mxx(nm_dev,nm_dev) , source=czero)
+        allocate( Sxx(nm_dev,nm_dev) , source=czero)
+        allocate( Kxx(nm_dev,nm_dev) , source=czero)
+        allocate( A(nm_dev,nm_dev) , source=czero)        
         print *,'  start computation L0_xx'
         !$omp parallel default(shared) private(i,j,ie,isub)
         !$omp do
@@ -2000,8 +2001,8 @@ module bse_dense
         enddo
         !$omp end do
         !$omp end parallel         
-        L0xx = L0xx * spindeg
-        Kxx(:,:) = - c1i*V(:,:)
+        L0xx = L0xx 
+        Kxx(:,:) = - c1i*V(:,:)*spindeg
         do i=1,nm_dev
             Kxx(i,i) = Kxx(i,i) + c1i*W(i,i)
         enddo
@@ -2012,8 +2013,6 @@ module bse_dense
             A(i,i) = A(i,i) + cone
         enddo
         !
-        Sxx = czero
-        Mxx = czero  
         print *,'  start computation S and M'
         !$omp parallel default(shared) private(i,j,k,l,Qijkl,L0Kdd,p,ie,isub)
         !$omp do
@@ -2021,7 +2020,7 @@ module bse_dense
             do j=1,nm_dev
                 do k=1,nm_dev
                     do l=1,nm_dev
-                        ! if (k/=l) then
+                        if (k/=l) then
                             if ((abs(i-k)<=ndiag).and.(abs(j-l)<=ndiag).and.(abs(j-k)<=ndiag).and.&
                                 (abs(i-l)<=ndiag).and.(abs(i-j)<=ndiag).and.(abs(k-l)<=ndiag)) then 
                                 Qijkl=czero
@@ -2034,10 +2033,9 @@ module bse_dense
                                                                 - G_lesser(i,l,ie,isub,ik) * G_greater(k,i,ie-nop,isub,ikd) * G_lesser(l,j,ie,isub,ik) * G_greater(j,k,ie-nop,isub,ikd) )* weights(isub)
                                                                                             
                                     enddo
-                                enddo
-                                Qijkl = - c1i * Qijkl 
+                                enddo                                
                                 !
-                                Qijkl = Qijkl * W(k,l) 
+                                Qijkl = - c1i * Qijkl * W(k,l) 
                                 L0Kdd=czero
                                 do isub=1,nsub
                                     do ie=nop+1,nen
@@ -2048,17 +2046,16 @@ module bse_dense
                                                                 - G_lesser(k,k,ie,isub,ik) * G_greater(l,l,ie-nop,isub,ikd)   )* weights(isub) 
                                                                                                 
                                     enddo
-                                enddo
-                                L0Kdd = L0Kdd 
+                                enddo                                
                                 !
                                 L0Kdd = cone - c1i * L0Kdd * W(k,l) 
                                 Qijkl = Qijkl / L0Kdd
                                 Sxx(i,j) = Sxx(i,j) + Qijkl
-                                do p=1,nm_dev
-                                    Mxx(i,p) = Mxx(i,p) - c1i * Qijkl * ( W(j,j) - V(j,p) ) 
+                                do p=1,nm_dev                                    
+                                    Mxx(i,p) = Mxx(i,p) - c1i * Qijkl * ( W(j,j) - V(j,p)*spindeg )                                     
                                 enddo
                             endif
-                        ! endif
+                        endif
                     enddo
                 enddo
             enddo
@@ -2077,11 +2074,31 @@ module bse_dense
         print *,'  start computation Axx^{-1}'
         call invert_inplace(A,nm_dev)
         Sxx(:,:) = L0xx(:,:) - Sxx(:,:)
-        call zgemm('n','n',nm_dev,nm_dev,nm_dev,cone,A,nm_dev,Sxx,nm_dev,czero,L0xx,nm_dev) 
-        P_retarded = - c1i * L0xx
+        call zgemm('n','n',nm_dev,nm_dev,nm_dev,cone,A,nm_dev,Sxx,nm_dev,czero,Mxx,nm_dev) 
+        P_retarded = - c1i * Mxx
         !
         ! call save_matrix('pr.dat',nm_dev, P_retarded)  
         !
+        ! ! calculate RPA epsilon and output to file
+        call zgemm('n','n',nm_dev,nm_dev,nm_dev,c1i,V,nm_dev,L0xx(1:nm_dev,1:nm_dev),nm_dev,czero,A,nm_dev) 
+        do j=1,nm_dev 
+            A(j,j) = A(j,j) + cone
+        enddo      
+        call invert_inplace(A,nm_dev)        
+        open(unit=99,file='rpa_epsilonM.dat',status='unknown', position="append", action="write")    
+        epsM = sum( A(nm_dev/2,1:nm_dev) )
+        write(99,*) dble(nop)*(En(2)-En(1)) , - aimag(epsM), dble(epsM) ! - Im \epsilon^{-1}
+        close(99)
+        !
+        ! ! calculate BSE epsilon_M and output to file        
+        call zgemm('n','n',nm_dev,nm_dev,nm_dev,-cone,V,nm_dev,P_retarded,nm_dev,czero,A,nm_dev) 
+        do j=1,nm_dev 
+            A(j,j) = A(j,j) + cone
+        enddo      
+        open(unit=99,file='bse_epsilonM.dat',status='unknown', position="append", action="write")    
+        epsM = sum( A(nm_dev/2,1:nm_dev) )
+        write(99,*) dble(nop)*(En(2)-En(1)) , aimag(epsM), dble(epsM) ! Im \epsilon_M -> absorption
+        close(99)
         deallocate(L0xx,Mxx,Sxx,Kxx,A)
     end subroutine bse_solve
 
--- f2py/tests/agnr7/test-gw-bse.py
+++ f2py/tests/agnr7/test-gw-bse.py
@@ -20,8 +20,8 @@ if __name__=='__main__':
 
    ns = 2
    length = 6 
-   nen = 200
-   nsub = 3
+   nen = 500
+   nsub = 5
    nky=1
    nkz=1
    nk=nky*nkz
@@ -58,27 +58,33 @@ if __name__=='__main__':
    lead_coupling[0:nb*ns,0:nb*ns,0,0] = lead_h10[:,:,0,0]
    lead_coupling[0:nb*ns,nb*(length-ns):nb*length,1,0] = lead_h10[:,:,1,0]
 
-   sig_r = np.zeros((nb*length,nb*length,nen,nk), dtype='complex')
-   sig_l = np.zeros((nb*length,nb*length,nen,nk), dtype='complex')
-   sig_g = np.zeros((nb*length,nb*length,nen,nk), dtype='complex')
+   # sig_r = np.zeros((nb*length,nb*length,nen,nk), dtype='complex')
+   # sig_l = np.zeros((nb*length,nb*length,nen,nk), dtype='complex')
+   # sig_g = np.zeros((nb*length,nb*length,nen,nk), dtype='complex')
+   
+   ndiag=nb*2
 
    G_retarded,G_lesser,G_greater,W0 = gf_dense.solve_gw_3d(niter=niter,nm_dev=nb*length,lx=Lx,length=length,spindeg=2.0,
                                                        temps=temp[0],tempd=temp[1],mus=mu[0],mud=mu[1],alpha_mix=0.5,
                                                        nen=nen,nsub=nsub,en=energies,nb=nb,ns=ns,nphiy=nky,nphiz=nkz,
                                                        ham=ham,h00lead=lead_h00,h10lead=lead_h10,t=lead_coupling,v=v,
-                                                       ndiag=nb,flatband=False)
+                                                       ndiag=ndiag,flatband=False)
 
    nop=100         
-   P_retarded1,system1,epsilon1,L1,M1 = bse_dense.bse_fullsolve(alpha=0.99,spindeg=2.0,ndiag=nb,nm_dev=nb*length,nen=nen,nsub=nsub,en=energies,nop=nop,nk=nk,g_lesser=G_lesser,g_greater=G_greater,g_retarded=G_retarded,w=W0[:,:,0],v=v[:,:,0])                                                        
+   P_retarded1,system1,epsilon1,L1,M1 = bse_dense.bse_fullsolve(alpha=0.99,spindeg=2.0,ndiag=ndiag,nm_dev=nb*length,nen=nen,nsub=nsub,en=energies,nop=nop,nk=nk,g_lesser=G_lesser,g_greater=G_greater,g_retarded=G_retarded,w=W0[:,:,0],v=v[:,:,0])                                                        
    plt.spy(system1)
    plt.savefig('pattern1.png')   
    # plt.show()
    plt.spy(L1)
    plt.savefig('L-pattern1.png')  
    plt.spy(M1)
-   plt.savefig('M-pattern1.png')  
+   plt.savefig('M-pattern1.png')     
+   plt.matshow(np.real(M1))
+   plt.show()
+   plt.matshow(np.imag(M1))
+   plt.show()
 
-   P_retarded2,system2,epsilon2 = bse_dense.bse_fullsolve_orig(alpha=0.99,spindeg=2.0,ndiag=nb,nm_dev=nb*length,nen=nen,nsub=nsub,en=energies,nop=nop,nk=nk,g_lesser=G_lesser,g_greater=G_greater,g_retarded=G_retarded,w=W0[:,:,0],v=v[:,:,0])                                                        
+   P_retarded2,system2,epsilon2 = bse_dense.bse_fullsolve_orig(alpha=0.99,spindeg=2.0,ndiag=ndiag,nm_dev=nb*length,nen=nen,nsub=nsub,en=energies,nop=nop,nk=nk,g_lesser=G_lesser,g_greater=G_greater,g_retarded=G_retarded,w=W0[:,:,0],v=v[:,:,0])                                                        
    plt.spy(system2)
    plt.savefig('pattern2.png')   
    
@@ -88,18 +94,26 @@ if __name__=='__main__':
    print('Max element in 2=', np.max(np.abs(P_retarded2)))
 
 
-   P_retarded3 = bse_dense.bse_solve(alpha=0.99,spindeg=2.0,ndiag=nb,nm_dev=nb*length,nen=nen,nsub=nsub,en=energies,nop=nop,nk=nk,g_lesser=G_lesser,g_greater=G_greater,g_retarded=G_retarded,w=W0[:,:,0],v=v[:,:,0])                                                        
+   P_retarded3 = bse_dense.bse_solve(alpha=0.99,spindeg=2.0,ndiag=ndiag,nm_dev=nb*length,nen=nen,nsub=nsub,en=energies,nop=nop,nk=nk,g_lesser=G_lesser,g_greater=G_greater,g_retarded=G_retarded,w=W0[:,:,0],v=v[:,:,0])                                                        
    print('Max error=', np.max(np.abs(P_retarded3-P_retarded2)))
    print('Max element in 3=', np.max(np.abs(P_retarded3)))
+
+   dE = energies[1] - energies[0]
+   for nop in range(10,int(4.0/dE)):
+       print( nop, dE*nop )
+       P_retarded3 = bse_dense.bse_fullsolve(alpha=0.99,spindeg=2.0,ndiag=ndiag,nm_dev=nb*length,
+                                         nen=nen,nsub=nsub,en=energies,nop=nop,nk=nk,
+                                         g_lesser=G_lesser,g_greater=G_greater,g_retarded=G_retarded,
+                                         w=W0[:,:,0],v=v[:,:,0])                                                        
    
   
-   blocksize = nb*length
-   ndiag = nb
-   A = system1
+   # blocksize = nb*length
+   # ndiag = nb
+   # A = system1
    
-   nn = blocksize*(ndiag*2-2)
+   # nn = blocksize*(ndiag*2-2)
 
-   A = A[-nn:,-nn:]
+   # A = A[-nn:,-nn:]
 
-   np.savez('system.npz', A=A, ndiag=ndiag,blocksize=blocksize)
+   # np.savez('system.npz', A=A, ndiag=ndiag,blocksize=blocksize)
    
