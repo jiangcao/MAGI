034a0803d8f31b0e5d132bd4d93c43f2f6285724
Jiang Cao <jiang.cao89@gmail.com>
2024-05-31 01:17:03 +0200

    fix compilation

--- f2py/run_f2py.sh
+++ f2py/run_f2py.sh
@@ -1,8 +1,10 @@
-F90FLAGS="-ffree-line-length-none -fbounds-check -fbacktrace -fopenmp"
+
+F90FLAGS="-g -march=native -O2 -ffree-line-length-none -fbounds-check -fbacktrace -ffast-math -fopenmp "
+
 rm src/*.mod
 rm src/*.o
 rm *.mod *.o
 gfortran -c mkl_dfti.f90
-f2py -c --fcompiler=gnu95 -m util src/util.f95 --f90flags="${F90FLAGS}" 
-f2py -c --fcompiler=gnu95 -m wannier src/wannier.f95 --f90flags="${F90FLAGS}"
-f2py -c --fcompiler=gnu95 -m negf src/negf.f95  skip: trimul_c mul_c : --f90flags="${F90FLAGS}"
+f2py -c  --fcompiler=gnu95 -m util src/util.f95 --f90flags="${F90FLAGS}" -lgomp -llapack -lblas
+f2py -c  --fcompiler=gnu95 -m wannier src/wannier.f95 --f90flags="${F90FLAGS}" -lgomp -llapack -lblas
+f2py -c --fcompiler=gnu95 -m negf src/negf.f95  skip: trimul_c mul_c : --f90flags="${F90FLAGS}" -lgomp -llapack -lblas
